db5cb001894365cc6e1294bb6bdcc3f0f397d85f
Gumani Dakalo Luvhengo <dakalo.luvhengo17@gmail.com>
2024-01-09 12:30:20 +0200

    refactoring

--- src/main/java/com/calculator/grade/studentgradecalculator/MainApplication.java
+++ src/main/java/com/calculator/grade/studentgradecalculator/MainApplication.java
@@ -11,8 +11,9 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 630, 240);
         stage.setTitle("Hello!");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
